b3fc8c1b22c5395d93b90d17e49b4c286157af59
azathoth449867 <azathoth0527689@gmail.com>
2025-11-04 15:17:18 -0500

    interface graphic de base

--- vue.py
+++ vue.py
@@ -1,1 +1,26 @@
+import tkinter as tk
+
+class Vue():
+    def __init__(self, parent, largeur, hauteur):
+        self.parent = parent
+        self.largeur = largeur
+        self.hauteur = hauteur
+        self.root = tk.Tk()
+        self.creer_interface()
+
+    def creer_interface(self):
+        self.cadre_principale = tk.Frame(self.root) 
+        self.cadre_principale.pack()
+
+        self.canvas = tk.Canvas(self.cadre_principale, width=self.largeur, height=self.hauteur)
+        self.canvas.pack()
+        self.canvas.create_rectangle(0,0, self.largeur, self.hauteur, fill="black")
+
+        self.canvas.create_rectangle(50,50, self.largeur - 50, self.hauteur -50, fill="white")
+
+vue = Vue(12, 450, 450)
+vue.root.mainloop()
+
+
+
 
