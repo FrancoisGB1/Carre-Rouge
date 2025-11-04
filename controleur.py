d7564c10766ef2128009c25a7a0ed4c566a3df49
Tran Kevin <e.ktran@etu.cvm.qc.ca>
2025-11-04 15:12:00 -0500

    test commit

--- controleur.py
+++ controleur.py
@@ -1,2 +1,14 @@
+import modele as mod
+import vue
 
-    
+class Controleur():
+    def __init__(self):
+        self.modele = Modele(self, largeur=450, hauteur=450)
+        self.vue = Vue(self, largeur=450, hauter=450)
+        self.vue.root.mainloop()
+
+    def demarrer_partie(self):
+        self.modele.demarrer_partie()
+
+if __name__ == "__main__":
+    c = Controleur()
---
+++ modele.py
@@ -0,0 +1,8 @@
+class Carre():
+    def __init__(self, parent, pos_x, pos_y):
+
+class Rectangle():
+    def __init__(self, parent, pos_x, pos_y):
+
+class Modele():
+    def __init__(self, parent, largeur, hauteur):
