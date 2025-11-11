dca4e4c697d3d01b9099c91f02d6e6436a33660e
kevin <spamventallday@gmail.com>
2025-11-10 19:28:57 -0500

    fixer les binds

--- modele.py
+++ modele.py
@@ -5,10 +5,6 @@ class Carre():
         self.hauteur = 40
         self.pos_x = 255
         self.pos_y = 255
-        self.cote_haut = self.pos_y - self.hauteur 
-        self.cote_bas = self.pos_y + self.hauteur 
-        self.cote_gauche = self.pos_x - self.largeur
-        self.cote_droite = self.pos_x + self.largeur
 
 class Rectangle():
     def __init__(self, parent, largeur, hauteur, pos_x, pos_y, or_x, or_y):
@@ -17,7 +13,7 @@ class Rectangle():
         self.hauteur = hauteur
         self.pos_x = pos_x
         self.pos_y = pos_y
-        self.vitesse = 4
+        
         self.orientation_x = or_x
         self.orientation_y = or_y
         self.orientations = {
@@ -26,6 +22,12 @@ class Rectangle():
             'haut-droit' : (1,-1),
             'haut-gauche' : (-1,-1),
         }
+        
+    def deplacer(self):
+        x,y = self.orientations['bas-droit']
+        self.pos_x = self.pos_x + (self.vitesse * x)
+        self.pos_y = self.pos_y + (self.vitesse * y)
+
 
 class Modele():
     def __init__(self, parent, largeur, hauteur):
@@ -36,6 +38,7 @@ class Modele():
         self.joueur = None
         self.scores = {} # key: name, value: score
         self.enJeu = False
+        self.vitesse = 5
 
     def initialiser_partie(self):
         self.rectangles = []
@@ -49,8 +52,8 @@ class Modele():
     def creer_rectangles(self):
         recGauche = Rectangle(self, 60, 60, 100, 100, 1, 1)
         recSupDroit = Rectangle(self, 60, 50, 300, 85, -1, 1)
-        recInfGauche = Rectangle(self, 30, 60, 85, 350, 1, 1)
-        recInfDroit = Rectangle(self, 100, 20, 355, 340, -1, -1)
+        recInfGauche = Rectangle(self, 30, 60, 85, 350, 1,1)
+        recInfDroit = Rectangle(self, 100, 20, 355, 340, -1, 1)
         self.rectangles.append(recGauche)
         self.rectangles.append(recSupDroit)
         self.rectangles.append(recInfGauche)
@@ -58,16 +61,20 @@ class Modele():
 
     def bouger_rectangle(self):
         for rec in self.rectangles:
-            rec.pos_x += rec.orientation_x * rec.vitesse
-            rec.pos_y += rec.orientation_y * rec.vitesse
-    def bouger_carre(self, souris_x_root, souris_y_root):
-        difference_x = self.pos_x - souris_x_root
-        difference_y = self.pos_y - souris_y_root
-        self.pos_x += difference_x + souris_x_root
-        self.pos_y += difference_y + souris_y_root
-    def collision(self):
+            rec.pos_x += rec.orientation_x * self.vitesse
+            rec.pos_y += rec.orientation_y * self.vitesse
+    def bouger_carre(self):
         pass
+    def collision(self):
+        for rec in self.rectangles:
+            if rec.pos_x + rec.largeur / 2 >= 450 or rec.pos_x - rec.largeur / 2 <= 0:
+                rec.orientation_x *= -1
+            if rec.pos_y + rec.hauteur / 2 >= 450 or rec.pos_y - rec.hauteur / 2 <= 0:
+                rec.orientation_y *= -1
+
         
-    
+        
+        
+
     def demarrer_partie(self, evt):
         pass
